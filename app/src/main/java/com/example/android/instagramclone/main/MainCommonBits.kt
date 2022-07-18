f19bc180ff8b563cf70a0b2ad622d945a9abac7d
Young Kim <deroyz@hotmail.com>
2022-07-18 14:48:38 -0600

    25 & 26 - User Image Card + Progress Spinner for Posts Screen

--- app/src/main/java/com/example/android/instagramclone/main/MainCommonBits.kt
+++ app/src/main/java/com/example/android/instagramclone/main/MainCommonBits.kt
@@ -1,11 +1,12 @@
 package com.example.android.instagramclone.main
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -14,10 +15,17 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.ImagePainter
+import coil.compose.rememberImagePainter
 import com.example.android.instagramclone.DestinationScreen
 import com.example.android.instagramclone.IgViewModel
+import com.example.android.instagramclone.R
 
 @Composable
 fun NotificationMessage(vm: IgViewModel) {
@@ -55,10 +63,48 @@ fun navigateTo(navController: NavController, dest: DestinationScreen) {
 fun CheckSignedIn(vm: IgViewModel, navController: NavController) {
     val alreadyLoggedIn = remember { mutableStateOf(false) }
     val signedIn = vm.signedIn.value
-    if(signedIn && !alreadyLoggedIn.value){
+    if (signedIn && !alreadyLoggedIn.value) {
         alreadyLoggedIn.value = true
-        navController.navigate(DestinationScreen.Feed.route){
+        navController.navigate(DestinationScreen.Feed.route) {
             popUpTo(0)
+        }
+    }
+}
+
+@Composable
+fun CommonImage(
+    data: String?,
+    modifier: Modifier = Modifier.wrapContentSize(),
+    contentScale: ContentScale = ContentScale.Crop
+) {
+    val painter = rememberImagePainter(data = data)
+    Image(
+        painter = painter,
+        contentDescription = null,
+        modifier = modifier,
+        contentScale = contentScale
+    )
+    if (painter.state is ImagePainter.State.Loading) {
+        CommonProgressSpinner()
+    }
+}
+
+@Composable
+fun UserImageCard(
+    userImage: String?,
+    modifier: Modifier = Modifier
+        .padding(8.dp)
+        .size(64.dp)
+) {
+    Card(shape = CircleShape, modifier = modifier) {
+        if (userImage.isNullOrEmpty()) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_user),
+                contentDescription = null,
+                colorFilter = ColorFilter.tint(Color.Gray)
+            )
+        } else {
+            CommonImage(data = userImage)
         }
     }
 }
--- app/src/main/java/com/example/android/instagramclone/main/MyPostsScreen.kt
+++ app/src/main/java/com/example/android/instagramclone/main/MyPostsScreen.kt
@@ -1,24 +1,24 @@
 package com.example.android.instagramclone.main
 
-import android.view.RoundedCorner
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ButtonColors
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.android.instagramclone.IgViewModel
+import com.example.android.instagramclone.R
 
 @Composable
 fun MyPostScreen(navController: NavController, vm: IgViewModel) {
@@ -30,7 +30,7 @@ fun MyPostScreen(navController: NavController, vm: IgViewModel) {
 //    }
 
     val userData = vm.userData.value
-    val isLoaindg = vm.inProgress.value
+    val isLoading = vm.inProgress.value
 
     Column {
         Column(modifier = Modifier.weight(1f)) {
@@ -93,10 +93,39 @@ fun MyPostScreen(navController: NavController, vm: IgViewModel) {
             navController = navController
         )
     }
+
+    if(isLoading){
+        CommonProgressSpinner()
+    }
 }
 
 
 @Composable
 fun ProfileImage(imageUrl: String?, onClick: () -> Unit) {
-
+    Box(modifier = Modifier
+        .clickable { onClick.invoke() }
+        .padding(top = 16.dp)) {
+        UserImageCard(
+            userImage = imageUrl, modifier = Modifier
+                .padding(8.dp)
+                .size(80.dp)
+        )
+        Card(
+            shape = CircleShape,
+            border = BorderStroke(
+                width = 2.dp,
+                color = Color.White,
+            ),
+            modifier = Modifier
+                .size(32.dp)
+                .align(Alignment.BottomEnd)
+                .padding(bottom = 8.dp, end = 8.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_add),
+                contentDescription = null,
+                modifier = Modifier.background(Color.Blue)
+            )
+        }
+    }
 }
