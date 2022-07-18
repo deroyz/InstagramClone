51bd1bb63e8ad67fac1a01ab8a86b40bf34e6822
Young <deroyz@hotmail.com>
2022-07-17 21:10:15 -0600

    20. Login screen UI

--- app/src/main/java/com/example/android/instagramclone/auth/SignupScreen.kt
+++ app/src/main/java/com/example/android/instagramclone/auth/SignupScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -30,10 +31,13 @@ import com.example.android.instagramclone.main.navigateTo
 
 @Composable
 fun SignupScreen(navController: NavController, vm: IgViewModel) {
+
+    val focus = LocalFocusManager.current
+
     Box(modifier = Modifier.fillMaxSize()) {
         Column(
             modifier = Modifier
-                .fillMaxSize()
+                .fillMaxWidth()
                 .wrapContentHeight()
                 .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally
@@ -82,11 +86,12 @@ fun SignupScreen(navController: NavController, vm: IgViewModel) {
 
             Button(
                 onClick = {
-                          vm.onSignup(
-                              usernameState.value.text,
-                              emailState.value.text,
-                              passState.value.text
-                          )
+                    focus.clearFocus(force = true)
+                    vm.onSignup(
+                        usernameState.value.text,
+                        emailState.value.text,
+                        passState.value.text
+                    )
                 },
                 modifier = Modifier.padding(8.dp)
             ) {
