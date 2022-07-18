e2598e65c7c484d8981cce74eb48ff6ad956c4b6
Young Kim <deroyz@hotmail.com>
2022-07-18 13:58:41 -0600

    24. MyPosts Screen

--- app/src/main/java/com/example/android/instagramclone/main/MyPostsScreen.kt
+++ app/src/main/java/com/example/android/instagramclone/main/MyPostsScreen.kt
@@ -1,22 +1,102 @@
 package com.example.android.instagramclone.main
 
+import android.view.RoundedCorner
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ButtonColors
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.android.instagramclone.IgViewModel
 
 @Composable
-fun MyPostScreen(navController: NavController, vm: IgViewModel){
-    Column(modifier = Modifier.fillMaxSize()) {
+fun MyPostScreen(navController: NavController, vm: IgViewModel) {
+//    Column(modifier = Modifier.fillMaxSize()) {
+//        Column(modifier = Modifier.weight(1f)) {
+//            Text(text = "MyPosts screen")
+//        }
+
+//    }
+
+    val userData = vm.userData.value
+    val isLoaindg = vm.inProgress.value
+
+    Column {
         Column(modifier = Modifier.weight(1f)) {
-            Text(text = "MyPosts screen")
+            Row() {
+                ProfileImage(userData?.imageUrl) {
+
+                }
+                Text(
+                    text = "15\nposts",
+                    modifier = Modifier
+                        .weight(1f)
+                        .align(Alignment.CenterVertically),
+                    textAlign = TextAlign.Center
+                )
+                Text(
+                    text = "55\nfollowers",
+                    modifier = Modifier
+                        .weight(1f)
+                        .align(Alignment.CenterVertically),
+                    textAlign = TextAlign.Center
+                )
+                Text(
+                    text = "93\nfollowing",
+                    modifier = Modifier
+                        .weight(1f)
+                        .align(Alignment.CenterVertically),
+                    textAlign = TextAlign.Center
+                )
+            }
+            Column(modifier = Modifier.padding(8.dp)) {
+                val usernameDisplay =
+                    if (userData?.username == null) "" else "@${userData.username}"
+                Text(text = userData?.name ?: "", fontWeight = FontWeight.Bold)
+                Text(text = usernameDisplay)
+                Text(text = userData?.bio ?: "")
+            }
+            OutlinedButton(
+                onClick = { },
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color.Transparent),
+                elevation = ButtonDefaults.elevation(
+                    defaultElevation = 0.dp,
+                    pressedElevation = 0.dp,
+                    disabledElevation = 0.dp,
+                ),
+                shape = RoundedCornerShape(10)
+            ) {
+                Text(text = "Edit Profile", color = Color.Black)
+            }
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Posts list")
+            }
         }
+
+
         BottomNavigationMenu(
             selectedItem = BottomNavigationItem.POSTS,
             navController = navController
         )
     }
+}
+
+
+@Composable
+fun ProfileImage(imageUrl: String?, onClick: () -> Unit) {
+
 }
