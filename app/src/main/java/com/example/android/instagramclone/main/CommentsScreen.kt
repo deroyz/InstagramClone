22a4d332e4ba66079c18c8518fc54f2a37a100fd
Young <deroyz@hotmail.com>
2022-07-22 23:38:03 -0600

    52. Display comments

--- app/src/main/java/com/example/android/instagramclone/main/CommentsScreen.kt
+++ app/src/main/java/com/example/android/instagramclone/main/CommentsScreen.kt
@@ -2,6 +2,8 @@ package com.example.android.instagramclone.main
 
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -10,10 +12,14 @@ import androidx.navigation.NavController
 import com.example.android.instagramclone.IgViewModel
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.example.android.instagramclone.data.CommentData
+import org.w3c.dom.Comment
 
 @Composable
 fun CommentsScreen(navController: NavController, vm: IgViewModel, postId: String) {
@@ -21,10 +27,43 @@ fun CommentsScreen(navController: NavController, vm: IgViewModel, postId: String
     var commentText by rememberSaveable { mutableStateOf("") }
     val focusManager = LocalFocusManager.current
 
+    val comments = vm.comments.value
+    val commentsProgress = vm.commentsProgress.value
+
     Column(modifier = Modifier.fillMaxSize()) {
-        Column(modifier = Modifier.weight(1f)) {
-            Text(text = "comments")
+
+        // Setup spinner for comments loading
+        if (commentsProgress) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CommonProgressSpinner()
+            }
+
+            // Case no comments on the post
+        } else if (comments.isEmpty()) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "No comments available")
+            }
+
+            // Regular comments display
+        } else {
+            LazyColumn(modifier = Modifier.weight(1f)) {
+                items(items = comments) { comment ->
+                    CommentRow(comment)
+                }
+            }
         }
+
+
         Row(
             modifier = Modifier
                 .fillMaxWidth()
@@ -54,5 +93,15 @@ fun CommentsScreen(navController: NavController, vm: IgViewModel, postId: String
 
             }
         }
+    }
+}
+
+@Composable
+fun CommentRow(comment: CommentData) {
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .padding(8.dp)){
+        Text(text = comment.username ?: "", fontWeight = FontWeight.Bold)
+        Text(text = comment.text ?: "", modifier = Modifier.padding(start = 8.dp))
     }
 }
